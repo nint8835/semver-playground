5117481f47c404e665e468b421e67127efdb6cb6
Riley Flynn <riley@rileyflynn.me>
2023-08-25 21:22:17 -0230

    Basic version parsing function

--- wasm/go.mod
+++ wasm/go.mod
@@ -1,3 +1,5 @@
 module github.com/nint8835/semver-playground/wasm
 
 go 1.21.0
+
+require github.com/Masterminds/semver/v3 v3.2.1
--- wasm/main.go
+++ wasm/main.go
@@ -1,19 +1,67 @@
 package main
 
 import (
-	"fmt"
 	"syscall/js"
+
+	"github.com/Masterminds/semver/v3"
 )
 
-func helloWorld() js.Func {
-	return js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-		fmt.Println("Hello World")
-		return nil
+func versionToMap(version *semver.Version) map[string]any {
+	return map[string]any{
+		"original":   version.Original(),
+		"major":      version.Major(),
+		"minor":      version.Minor(),
+		"patch":      version.Patch(),
+		"prerelease": version.Prerelease(),
+		"metadata":   version.Metadata(),
+	}
+}
+
+func versionFromMap(version map[string]any) *semver.Version {
+	return semver.New(
+		version["major"].(uint64),
+		version["minor"].(uint64),
+		version["patch"].(uint64),
+		version["prerelease"].(string),
+		version["metadata"].(string),
+	)
+}
+
+func promisify(originalFunc func(this js.Value, args []js.Value) (any, error)) js.Func {
+	return js.FuncOf(func(this js.Value, args []js.Value) any {
+		handler := js.FuncOf(func(_ js.Value, innerArgs []js.Value) any {
+			resolve := innerArgs[0]
+			reject := innerArgs[1]
+
+			go func() {
+				result, err := originalFunc(this, args)
+				if err != nil {
+					errorConstructor := js.Global().Get("Error")
+					errorObject := errorConstructor.New(err.Error())
+					reject.Invoke(errorObject)
+				} else {
+					resolve.Invoke(js.ValueOf(result))
+				}
+			}()
+
+			return nil
+		})
+
+		promiseConstructor := js.Global().Get("Promise")
+		return promiseConstructor.New(handler)
 	})
+}
+
+func parseVersionFunc(this js.Value, args []js.Value) (any, error) {
+	version, err := semver.StrictNewVersion(args[0].String())
+	if err != nil {
+		return nil, err
+	}
+	return versionToMap(version), nil
 }
 
 func main() {
 	ch := make(chan struct{}, 0)
-	js.Global().Set("helloWorld", helloWorld())
+	js.Global().Set("parseVersion", promisify(parseVersionFunc))
 	<-ch
 }
