e83b6b969592bc01070077e36bd0f2e37e9c0f47
Riley Flynn <riley@rileyflynn.me>
2023-08-26 18:47:24 -0230

    Type fix

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,15 +1,13 @@
 /// <reference types="vite/client" />
 /// <reference types="@types/golang-wasm-exec" />
 
-declare global {
-    type Version = {
-        major: number;
-        minor: number;
-        patch: number;
-        prerelease: string;
-        metadata: string;
-        original: string;
-    };
-}
+declare type Version = {
+    major: number;
+    minor: number;
+    patch: number;
+    prerelease: string;
+    metadata: string;
+    original: string;
+};
 
 declare function parseVersion(version: string): Promise<Version>;
