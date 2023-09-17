432f3f56b640b73be6c8385c84a854e0525c3981
Riley Flynn <riley@rileyflynn.me>
2023-09-17 03:28:10 -0230

    Display normalized query

--- frontend/src/components/ConstraintEditor.tsx
+++ frontend/src/components/ConstraintEditor.tsx
@@ -6,28 +6,41 @@ import Editor from './Editor';
 
 function ConstraintEditor(props: { constraintString: string; setConstraintString: (val: string) => void }) {
     const { constraintString, setConstraintString } = props;
+    const deferredConstraintString = useDeferredValue(constraintString);
     const constraintQuery = useDeferredValue(useQuery(getParseConstraintQuery(constraintString)));
 
     return (
         <Editor setEditorValue={setConstraintString} editorValue={constraintString} title="Version constraint">
             {constraintQuery.isFetched && (
-                <div
-                    className={`mt-2 flex flex-row items-center rounded-md transition-all ${
-                        constraintQuery.isSuccess ? 'bg-green-900' : 'bg-red-900'
-                    }`}
-                >
-                    {constraintQuery.isSuccess ? (
+                <>
+                    <div
+                        className={`mt-2 flex flex-row items-center rounded-md transition-all ${
+                            constraintQuery.isSuccess ? 'bg-green-900' : 'bg-red-900'
+                        }`}
+                    >
+                        {constraintQuery.isSuccess ? (
+                            <>
+                                <CheckIcon className="h-8 w-8" />
+                                <p>Constraint valid</p>
+                            </>
+                        ) : (
+                            <>
+                                <XMarkIcon className="h-8 w-8" />
+                                <p>Constraint invalid</p>
+                            </>
+                        )}
+                    </div>
+                    {constraintQuery.isSuccess && constraintQuery.data !== deferredConstraintString && (
                         <>
-                            <CheckIcon className="h-8 w-8" />
-                            <p>Constraint valid</p>
-                        </>
-                    ) : (
-                        <>
-                            <XMarkIcon className="h-8 w-8" />
-                            <p>Constraint invalid</p>
+                            <h3 className="mt-2 font-bold">Normalized constraint</h3>
+                            <code className="mt-2 inline-block rounded-md bg-zinc-900 p-1">{constraintQuery.data}</code>
+                            <div className="mt-2 italic text-zinc-400">
+                                The constraint you entered produced a different value after parsing. This normalized
+                                value is what will be used when comparing versions against the constraint.
+                            </div>
                         </>
                     )}
-                </div>
+                </>
             )}
         </Editor>
     );
